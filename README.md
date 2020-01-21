c9d807a77486c0c7f617a9b1a0d5126b67348460
Viktor Kronvall <viktor.kronvall@gmail.com>
2020-01-21 18:30:45 +0900

    Parse with commitment

---
+++ README.md
@@ -0,0 +1,6 @@
+# aeson-commit
+
+## Parse Aeson data with commitment
+
+Aeson parsers backtracks too much for some use cases. The commit
+parser forbids backtracking for already committed parses.
---
+++ src/Data/Aeson/Commit.hs
@@ -0,0 +1,81 @@
+{-# LANGUAGE DataKinds                  #-}
+{-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE FlexibleInstances          #-}
+{-# LANGUAGE GADTs                      #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE PolyKinds                  #-}
+{-# LANGUAGE RankNTypes                 #-}
+{-# LANGUAGE TypeFamilies               #-}
+{-# LANGUAGE TypeOperators              #-}
+module Data.Aeson.Commit
+    ( commit
+    , runCommit
+    , Commit
+    , parseKey
+    , matchKey
+    , parseCommit
+    , decodeJSONFile
+    , decodeYamlFile
+    , module Data.Aeson.Types
+    )
+where
+
+import Control.Applicative
+import Control.Monad ((>=>))
+import Control.Monad.Trans.Class
+import Control.Monad.Trans.Maybe
+import Control.Monad.Trans.Reader
+import Data.Aeson
+import Data.Aeson.Types hiding (parse)
+import Data.Bifunctor (first)
+import Data.Text (Text, unpack)
+import Data.Yaml (decodeFileEither, prettyPrintParseException)
+
+-- | A commit parser
+newtype Commit x = Commit { unCommit :: ReaderT Value (MaybeT Parser) x}
+  deriving (Functor, Applicative, Alternative, Monad)
+
+-- | Create a commit parser that doesn't backtrack if the first parser parses
+-- successfully.
+commit :: (Value -> Parser x) -> (x -> Parser y) -> Commit y
+commit f g = Commit . ReaderT $ MaybeT . pure . parseMaybe f >=> lift . g
+
+-- | Run a commit parser by picking the first matching parser that commits.
+-- The returned parser fails if no parser matches.
+runCommit :: Commit x -> Value -> Parser x
+runCommit go value = runMaybeT (runReaderT (unCommit go) value) >>= matched
+  where
+   matched Nothing  = fail $ "No parser matches value " <> show value
+   matched (Just y) = pure y
+
+-- | Parse a key by name in an object.
+parseKey
+  :: FromJSON a
+  => Text
+  -> Value
+  -> Parser a
+parseKey key = withObject (unpack key) $ \o -> o .: key
+
+-- | Match key against an object or a string.
+-- If the parsed Value is an object ensure that the object has the key.
+-- If the parsed Value is a string match the key against the string.
+matchKey
+  :: Text
+  -> Value
+  -> Parser ()
+matchKey key v = withObject (unpack key) (.: key) v
+  <|> withText (unpack key) (\txt ->
+      if key == txt
+        then pure ()
+        else fail $ "key mismatch got " <> unpack txt <> ", expected " <> unpack key
+      ) v
+
+parseCommit :: Commit t -> Value -> Either String t
+parseCommit parser = parseEither (runCommit parser)
+
+decodeJSONFile :: FilePath -> IO (Either String Value)
+decodeJSONFile = eitherDecodeFileStrict
+
+decodeYamlFile :: FilePath -> IO (Either String Value)
+decodeYamlFile = fmap (first prettyPrintParseException) . decodeFileEither
