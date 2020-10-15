816917a197d01b77006ebd91ef173fd69a8f17bf
Jonas Carpay <jonascarpay@gmail.com>
2020-10-15 17:41:38 +0900

    Update docs

--- src/Data/Aeson/Commit.hs
+++ src/Data/Aeson/Commit.hs
@@ -8,11 +8,14 @@ import           Data.Aeson.Types
 import           Data.Text            (Text)
 import           Data.Void            (Void, absurd)
 
--- | A parser that has _two_ failure modes; the 'ExceptT' or in the underlying 'Parser'.
---   The alternative instance only recovers from failures in the `ExceptT`.
---   This means that, as soon as we successfully construct a 'Right' value, the 'Alternative' considers the 'Commit' a success, even though the inner 'Parser' can still fail.
+-- | A 'Parser' that has _two_ failure modes; recoverable and non-recoverable.
+--   The default, recoverable failure is the equivalent to aeson's default 'Parser' behavior.
+--   The non-recoverable failure mode is used to commit to a branch; to commit means that every subsequent failure is non-recoverable.
 --
---   The 'Void' guarantees that that parser contains an error value.
+--   The implementation works by capturing failure in either the 'ExceptT' or in the underlying 'Parser'.
+--   The derived 'Alternative' instance will only recover from failures in the 'ExceptT'.
+--   This means that as soon as we successfully construct a 'Right' value, the 'Alternative' considers the 'Commit' a success, even though the underlying parser might have failed.
+--   The 'Void' represents the guarantee that we only collect error values.
 newtype Commit a = Commit {unCommit :: ExceptT [Parser Void] Parser a}
   deriving (Monad, Functor, Applicative, Alternative)
 
@@ -20,7 +23,7 @@ newtype Commit a = Commit {unCommit :: ExceptT [Parser Void] Parser a}
 --   If the first parser succeeds, the 'Commit' is a success, and any failures in the inner action will be preserved.
 commit :: Parser a -> (a -> Parser b) -> Commit b
 commit pre post = Commit $ do
-  a <- ExceptT $ captureError pre -- Lift pre's error to the ExceptT level
+  a <- ExceptT $ captureError pre
   lift $ post a
     where
       captureError :: Parser b -> Parser (Either [Parser Void] b)
@@ -42,22 +45,21 @@ runCommit (Commit f) = runExceptT f >>= either handleErrors pure
         -- `aeson` doesn't support such a thing. When errors are reported in `aeson`
         -- a single JSONPath defines how the error message is presented.
 
--- | Convenience wrapper around 'commit' for when the commit is simply checking whether a key is present in some object.
---   If it is, it will append the key to the JSONPath of the inner context through '<?>'.
---   This is should give the proper JSON path for error messages, although I'm not entirely sure if this is idiomatic.
+-- | Convenience wrapper around 'commit' for when the commit is checking whether a key is present in some object.
+--   If it is, it will commit and append the key to the JSONPath of the inner context through '<?>', which will give nicer error messages.
 (.:>)  :: FromJSON a => Object -> Text -> (a -> Parser b) -> Commit b
 (o .:> k) cont = commit (o .: k) (\v -> cont v <?> Key k)
 
 -- | Try to parse with a 'Parser' and commit if it parses successfully.
 --   Unlike 'liftParser', the parser's failure is recoverable.
---
--- > tryParser p = commit p pure
+--   
+-- > tryParser empty <|> p = p
 tryParser :: Parser a -> Commit a
 tryParser p = commit p pure
 
 -- | Turn a 'Parser' into a 'Commit'.
 --   Unlike 'tryParser', the parser's failure is _not_ recoverable, i.e. the parse is always committed.
---
--- > liftParser p = commit (pure ()) (\() -> p) = Commit (lift p)
+--   
+-- > liftParser empty <|> p = empty
 liftParser :: Parser a -> Commit a
 liftParser p = commit (pure ()) (const p)
