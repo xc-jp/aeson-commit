7b993f2589209a63f71b130cc571e388f589f8e4
Jonas Carpay <jonascarpay@gmail.com>
2021-10-13 19:11:23 +0900

    Ormolize

--- src/Data/Aeson/Commit.hs
+++ src/Data/Aeson/Commit.hs
@@ -1,47 +1,46 @@
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE CPP #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 
-{-|
-   Commitment mechanism for aeson 'Parser'.
-   Commitment means that if some initial parsing succeeds, subsequent failures are unrecoverable.
-   In this example, not having the key @nested@ is a normal, recoverable failure, and parsing will continue looking for another key.
-   However, if @nested@ is present but malformed, the entire parser fails.
-
-   > parse o = (o .:> "nested") (withObject "nestedObj" (.: "value"))
-   >         <|> tryParser (o .: "value")
-
-   > { value: "foo", otherField: "bar" }
-   > -> Right "foo"
-   >
-   > { value: "foo", nested: { value: "bar" } }
-   > -> Right "bar"
-   >
-   > { value: "foo", nested: { bar: 9 } }
-   > -> Left "Error in $.nested: key \"value\" not found"
-   >
-   > { value: "foo", nested: 9 }
-   > -> Left "Error in $.nested: parsing nestedObj failed, expected Object, but encountered Number"
-   >
-   > {}
-   > -> Left
-   >   "Error in $: No match,
-   >    - key \"value\" not found"
-   >    - key \"nested\" not found"
-
--}
+-- |
+--   Commitment mechanism for aeson 'Parser'.
+--   Commitment means that if some initial parsing succeeds, subsequent failures are unrecoverable.
+--   In this example, not having the key @nested@ is a normal, recoverable failure, and parsing will continue looking for another key.
+--   However, if @nested@ is present but malformed, the entire parser fails.
+--
+--   > parse o = (o .:> "nested") (withObject "nestedObj" (.: "value"))
+--   >         <|> tryParser (o .: "value")
+--
+--   > { value: "foo", otherField: "bar" }
+--   > -> Right "foo"
+--   >
+--   > { value: "foo", nested: { value: "bar" } }
+--   > -> Right "bar"
+--   >
+--   > { value: "foo", nested: { bar: 9 } }
+--   > -> Left "Error in $.nested: key \"value\" not found"
+--   >
+--   > { value: "foo", nested: 9 }
+--   > -> Left "Error in $.nested: parsing nestedObj failed, expected Object, but encountered Number"
+--   >
+--   > {}
+--   > -> Left
+--   >   "Error in $: No match,
+--   >    - key \"value\" not found"
+--   >    - key \"nested\" not found"
 module Data.Aeson.Commit
-  ( commit
-  , runCommit
-  , (.:>)
-  , tryParser
-  , liftParser
-  , Commit (..)
-  ) where
+  ( commit,
+    runCommit,
+    (.:>),
+    tryParser,
+    liftParser,
+    Commit (..),
+  )
+where
 
-import           Control.Applicative  (Alternative (..))
-import           Control.Monad.Except
-import           Data.Aeson.Types
-import           Data.Void            (Void, absurd)
+import Control.Applicative (Alternative (..))
+import Control.Monad.Except
+import Data.Aeson.Types
+import Data.Void (Void, absurd)
 #if MIN_VERSION_aeson(2,0,0)
 import qualified Data.Aeson.Key as Key
 #else
@@ -69,26 +68,26 @@ commit :: Parser a -> (a -> Parser b) -> Commit b
 commit pre post = Commit $ do
   a <- ExceptT $ captureError pre
   lift $ post a
-    where
-      captureError :: Parser b -> Parser (Either [Parser Void] b)
-      captureError p = Right <$> p <|> pure (Left [fmap (const undefined) p])
+  where
+    captureError :: Parser b -> Parser (Either [Parser Void] b)
+    captureError p = Right <$> p <|> pure (Left [fmap (const undefined) p])
 
 -- | Run a 'Commit', capturing its result in a 'Parser'.
 runCommit :: Commit a -> Parser a
 runCommit (Commit f) = runExceptT f >>= either handleErrors pure
   where
-  handleErrors :: [Parser Void] -> Parser a
-  handleErrors []     = fail "No parsers tried"
-  handleErrors (p:ps) = fmap absurd (go (p:ps) [] [])
-    where
-    go [] path errors = parserThrowError path ("No match,\n" <> unlines (fmap ("- " <>) errors))
-    go (y:ys) _ msgs = parserCatchError y $ \path msg ->
-      go ys path (msg:msgs)
+    handleErrors :: [Parser Void] -> Parser a
+    handleErrors [] = fail "No parsers tried"
+    handleErrors (p : ps) = fmap absurd (go (p : ps) [] [])
+      where
         -- TODO: how do we handle the multiple JSONPaths?
         -- Right now the rightmost failure's path is used when presenting
         -- the error message. Ideally one path per error would be preferable but
         -- `aeson` doesn't support such a thing. When errors are reported in `aeson`
         -- a single JSONPath defines how the error message is presented.
+        go [] path errors = parserThrowError path ("No match,\n" <> unlines (fmap ("- " <>) errors))
+        go (y : ys) _ msgs = parserCatchError y $ \path msg ->
+          go ys path (msg : msgs)
 
 -- | Convenience wrapper around 'commit' for when the commit is checking whether a key is present in some object.
 --   If it is, it will commit and append the key to the JSONPath of the inner context through '<?>', which will give nicer error messages.
@@ -102,7 +101,7 @@ runCommit (Commit f) = runExceptT f >>= either handleErrors pure
 
 -- | Turn a 'Parser' into a 'Commit'
 --   Unlike 'liftParser', the parser's failure is recoverable.
---   
+--
 -- > tryParser empty <|> p = p
 -- > tryParser p = commit p pure
 tryParser :: Parser a -> Commit a
@@ -110,7 +109,7 @@ tryParser p = commit p pure
 
 -- | Turn a 'Parser' into a 'Commit'.
 --   Unlike 'tryParser', the parser's failure is _not_ recoverable, i.e. the parse is always committed.
---   
+--
 -- > liftParser empty <|> p = empty
 -- > liftParser p = commit (pure ()) (const p)
 liftParser :: Parser a -> Commit a
--- test/tasty/Main.hs
+++ test/tasty/Main.hs
@@ -1,57 +1,60 @@
-{-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
 
 module Main (main) where
 
-import           Data.Aeson.QQ
-import           Data.Aeson.Types
-import           Data.Aeson.Commit
-import           Control.Applicative
-import           Data.Text (Text)
-import           Test.Tasty
-import           Test.Tasty.Hspec
+import Control.Applicative
+import Data.Aeson.Commit
+import Data.Aeson.QQ
+import Data.Aeson.Types
+import Data.Text (Text)
+import Test.Tasty
+import Test.Tasty.Hspec
 
 tests :: Spec
-tests = testParserWithCases pNested
-  [ ( "fails"
-    , [aesonQQ| {} |]
-    , Left $ unlines
-      [ "Error in $: No match,"
-      , "- key \"value\" not found"
-      , "- key \"nested\" not found"
-      ]
-    )
-  , ( "succeeds unnested"
-    , [aesonQQ| { value: "top" } |]
-    , Right "top"
-    )
-  , ( "succeeds and prefers nested"
-    , [aesonQQ| { value: "top" , nested: { value: "nest" } } |]
-    , Right "nest"
-    )
-  , ( "fails on malformed nested"
-    , [aesonQQ| { value: "top", nested: { foo: 9 } } |]
-    , Left "Error in $.nested: key \"value\" not found"
-    )
-  , ( "fails on nested type mismatch"
-    , [aesonQQ| { value: "top", nested: 9 } |]
-    , Left "Error in $.nested: parsing nestedObj failed, expected Object, but encountered Number"
-    )
-  ]
+tests =
+  testParserWithCases
+    pNested
+    [ ( "fails",
+        [aesonQQ| {} |],
+        Left $
+          unlines
+            [ "Error in $: No match,",
+              "- key \"value\" not found",
+              "- key \"nested\" not found"
+            ]
+      ),
+      ( "succeeds unnested",
+        [aesonQQ| { value: "top" } |],
+        Right "top"
+      ),
+      ( "succeeds and prefers nested",
+        [aesonQQ| { value: "top" , nested: { value: "nest" } } |],
+        Right "nest"
+      ),
+      ( "fails on malformed nested",
+        [aesonQQ| { value: "top", nested: { foo: 9 } } |],
+        Left "Error in $.nested: key \"value\" not found"
+      ),
+      ( "fails on nested type mismatch",
+        [aesonQQ| { value: "top", nested: 9 } |],
+        Left "Error in $.nested: parsing nestedObj failed, expected Object, but encountered Number"
+      )
+    ]
   where
     pNested :: Value -> Parser Text
     pNested = withObject "topLevel" $ \o ->
-      runCommit
-        $ (o .:> "nested") (withObject "nestedObj" (.: "value"))
-        <|> tryParser (o .: "value")
+      runCommit $
+        (o .:> "nested") (withObject "nestedObj" (.: "value"))
+          <|> tryParser (o .: "value")
 
-testParserWithCases
-  :: (Eq a, Show a)
-  => (v -> Parser a)
-  -> [(String, v, Either String a)]
-  -> Spec
+testParserWithCases ::
+  (Eq a, Show a) =>
+  (v -> Parser a) ->
+  [(String, v, Either String a)] ->
+  Spec
 testParserWithCases p =
-  mapM_ ( \(name, v, result) -> it name (parseEither p v `shouldBe` result))
+  mapM_ (\(name, v, result) -> it name (parseEither p v `shouldBe` result))
 
 main :: IO ()
 main = testSpecs tests >>= defaultMain . testGroup "aeson-commit"
